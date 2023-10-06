02127a21220157f163a2305990dbad77c7596ec4
ahm228 <94765616+ahm228@users.noreply.github.com>
2023-10-06 09:43:56 -0500

    Initial commit

---
+++ sudokuSolver.py
@@ -0,0 +1,53 @@
+def is_valid(board, row, col, num):
+    for i in range(9):
+        if board[row][i] == num:
+            return False
+        if board[i][col] == num:
+            return False
+    
+    startRow, startCol = 3 * (row // 3), 3 * (col // 3)
+    for i in range(startRow, startRow + 3):
+        for j in range(startCol, startCol + 3):
+            if board[i][j] == num:
+                return False
+    return True
+
+def solve(board):
+    empty = find_empty_cell(board)
+    if not empty:
+        return True
+    row, col = empty
+
+    for i in range(1, 10):
+        if is_valid(board, row, col, i):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+    return False
+
+def find_empty_cell(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+if __name__ == '__main__':
+    board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
+    solve(board)
+    for row in board:
+        print(row)
