df01554002b44a15a88e9d1c6bf96aa17ca47bab
ahm228 <94765616+ahm228@users.noreply.github.com>
2023-10-06 09:49:26 -0500

    Update sudokusolver.py

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,39 +1,44 @@
-def is_valid(board, row, col, num):
-    for i in range(9):
-        if board[row][i] == num:
+N = 9  # Board size
+M = 3  # Sub-grid size
+
+def isValid(board, row, col, num):
+    # Check in row, col, and the 3x3 box
+    startRow, startCol = M * (row // M), M * (col // M)
+    for i in range(N):
+        if board[row][i] == num or board[i][col] == num or board[startRow + i // M][startCol + i % M] == num:
             return False
-        if board[i][col] == num:
-            return False
-    
-    startRow, startCol = 3 * (row // 3), 3 * (col // 3)
-    for i in range(startRow, startRow + 3):
-        for j in range(startCol, startCol + 3):
-            if board[i][j] == num:
-                return False
     return True
 
 def solve(board):
-    empty = find_empty_cell(board)
+    empty = findEmptyCell(board)
     if not empty:
         return True
     row, col = empty
 
-    for i in range(1, 10):
-        if is_valid(board, row, col, i):
+    for i in range(1, N+1):
+        if isValid(board, row, col, i):
             board[row][col] = i
-
             if solve(board):
                 return True
-
             board[row][col] = 0
     return False
 
-def find_empty_cell(board):
-    for i in range(len(board)):
-        for j in range(len(board[0])):
+def findEmptyCell(board):
+    for i in range(N):
+        for j in range(N):
             if board[i][j] == 0:
                 return (i, j)
     return None
+
+def printBoard(board):
+    for i in range(N):
+        if i % M == 0 and i != 0:
+            print("-"*21)  # Print horizontal separator
+        for j in range(N):
+            if j % M == 0 and j != 0:
+                print("|", end=" ")  # Print vertical separator
+            print(board[i][j], end=" ")
+        print()
 
 if __name__ == '__main__':
     board = [
@@ -49,5 +54,4 @@ if __name__ == '__main__':
     ]
 
     solve(board)
-    for row in board:
-        print(row)
+    printBoard(board)
