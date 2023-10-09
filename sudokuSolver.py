dca9e45a50640471cd0480218b958c49f46235e7
ahm228 <94765616+ahm228@users.noreply.github.com>
2023-10-09 09:03:16 -0500

    Update sudokuSolver.py
    Introduction of isValid, placeNumber, and removeNumber functions.
    Modification of the solve and generateSudoku functions to make use of the new functions and the sets.
    Initialization of rows, cols, and boxes sets in the main section of the code.
    closes #8

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,34 +1,39 @@
 import random
 
-def isValid(board, row, col, num):
-    startRow, startCol = M * (row // M), M * (col // M)
-
-    for i in range(N):
-        if board[row][i] == num or board[i][col] == num or board[startRow + i // M][startCol + i % M] == num:
-            return False
-        
+def isValid(num, row, col):
+    boxIndex = (row // M) * M + col // M
+    if num in rows[row] or num in cols[col] or num in boxes[boxIndex]:
+        return False
     return True
+
+def placeNumber(board, num, row, col):
+    boxIndex = (row // M) * M + col // M
+    board[row][col] = num
+    rows[row].add(num)
+    cols[col].add(num)
+    boxes[boxIndex].add(num)
+
+def removeNumber(board, row, col):
+    boxIndex = (row // M) * M + col // M
+    removed = board[row][col]
+    board[row][col] = 0
+    rows[row].remove(removed)
+    cols[col].remove(removed)
+    boxes[boxIndex].remove(removed)
 
 def solve(board):
     empty = findEmptyCell(board)
-
     if not empty:
         return True
-    
     row, col = empty
-
-    nums = list(range(1, N+1))
+    nums = list(range(1, N + 1))
     random.shuffle(nums)
-
     for i in nums:
-        if isValid(board, row, col, i):
-            board[row][col] = i
-
+        if isValid(i, row, col):
+            placeNumber(board, i, row, col)
             if solve(board):
                 return True
-            
-            board[row][col] = 0
-
+            removeNumber(board, row, col)
     return False
 
 def findEmptyCell(board):
@@ -36,36 +41,36 @@ def findEmptyCell(board):
         for j in range(N):
             if board[i][j] == 0:
                 return (i, j)
-            
     return None
 
 def printBoard(board):
     for i in range(N):
         if i % M == 0 and i != 0:
-            print("-"*(2*N + M - 1))
-
+            for j in range(N):
+                print("─" * 3, end="")
+                if j % M == M - 1 and j != N - 1:
+                    print("┼", end="")
+            print()
         for j in range(N):
             if j % M == 0 and j != 0:
-                print("|", end=" ")
-            print(board[i][j], end=" ")
-
+                print("│", end="")
+            if board[i][j] != 0:
+                print(f" {board[i][j]} ", end="")
+            else:
+                print("   ", end="")
         print()
 
 def generateSudoku(difficulty):
     board = [[0]*N for _ in range(N)]
     solve(board)
-
     totalCells = N * N
     numToRemove = int(difficulty * totalCells)
-
     while numToRemove > 0:
         row = random.randint(0, N-1)
         col = random.randint(0, N-1)
-
         if board[row][col] != 0:
-            board[row][col] = 0
+            removeNumber(board, row, col)
             numToRemove -= 1
-
     return board
 
 def getUserInput():
@@ -73,32 +78,26 @@ def getUserInput():
         try:
             N = int(input("Enter board size (e.g., 9 for a 9x9 board): "))
             M = int(N**0.5)
-
             if M * M != N:
                 print("Board size must be a perfect square (e.g., 4, 9, 16,...).")
                 continue
-            
             difficulty = float(input("Enter difficulty (0 to 1, where 1 is hardest): "))
-
             if not (0 <= difficulty <= 1):
                 print("Difficulty must be between 0 and 1.")
                 continue
-
             return N, M, difficulty
-        
         except ValueError:
             print("Invalid input. Please try again.")
 
 if __name__ == '__main__':
     N, M, difficulty = getUserInput()
-    
+    rows = [set() for _ in range(N)]
+    cols = [set() for _ in range(N)]
+    boxes = [set() for _ in range(N)]
     board = generateSudoku(difficulty)
-
     print("Initial unsolved board:")
     printBoard(board)
     print("\n\n")
-
     solve(board)
-    
     print("Solved board:")
     printBoard(board)
